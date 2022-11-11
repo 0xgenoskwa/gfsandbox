626ee8e5f28c3272fff5a8ddfeaf5e778b976040
camthanh <cthanh@autonomous.nyc>
2022-11-11 12:58:12 +0700

    optimize start mqtt (3)

--- cmd/genframe/main.go
+++ cmd/genframe/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"go.genframe.xyz/internal/genframe"
 )
 
 func main() {
 	// InitializeServer
 	g := genframe.InitializeGenframe()
-	fmt.Println("genframe run 1")
 	g.Run()
-	fmt.Println("genframe run 2")
 }
